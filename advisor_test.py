3f8dfda9afbcc938e1eb0cebf1c78670a6267318
mr1301 <46725466+mr1301@users.noreply.github.com>
2019-05-08 15:09:23 -0400

    test if decipher response works

--- advisor_test.py
+++ advisor_test.py
@@ -1,6 +1,46 @@
 import os
 import pytest
-from advisor import to_usd
+from advisor import to_usd, decipher_response
 
 def test_to_usd():
     assert to_usd(5) == "$5.00"
+
+def test_decipher_response():
+    parsed_response = {
+        "Meta Data": {
+            "1. Information": "Daily Prices (open, high, low, close) and Volumes",
+            "2. Symbol": "MSFT",
+            "3. Last Refreshed": "2018-06-08",
+            "4. Output Size": "Full size",
+            "5. Time Zone": "US/Eastern"
+        },
+        "Time Series (Daily)": {
+            "2019-06-08": {
+                "1. open": "101.0924",
+                "2. high": "101.9500",
+                "3. low": "100.5400",
+                "4. close": "101.6300",
+                "5. volume": "22165128"
+            },
+            "2019-06-07": {
+                "1. open": "102.6500",
+                "2. high": "102.6900",
+                "3. low": "100.3800",
+                "4. close": "100.8800",
+                "5. volume": "28232197"
+            },
+            "2019-06-06": {
+                "1. open": "102.4800",
+                "2. high": "102.6000",
+                "3. low": "101.9000",
+                "4. close": "102.4900",
+                "5. volume": "21122917"
+            }
+        }
+    }
+    deciphered_response = [
+        {"timestamp": "2019-06-08", "open": 101.0924, "high": 101.95, "low": 100.54, "close": 101.63, "volume": 22165128},
+        {"timestamp": "2019-06-07", "open": 102.65, "high": 102.69, "low": 100.38, "close": 100.88, "volume": 28232197},
+        {"timestamp": "2019-06-06", "open": 102.48, "high": 102.60, "low": 101.90, "close": 102.49, "volume": 21122917},
+    ]
+    assert decipher_response(parsed_response) == deciphered_response
